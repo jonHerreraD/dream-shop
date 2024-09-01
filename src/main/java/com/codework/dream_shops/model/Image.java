9ff8cf6c5945301df4a08bed2e52ca4314d2f1c0
Jonathan <diazjonathan3009@gmail.com>
2024-09-01 15:49:39 -0600

    Model package and entities created

---
+++ src/main/java/com/codework/dream_shops/model/Image.java
@@ -0,0 +1,28 @@
+package com.codework.dream_shops.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Blob;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
